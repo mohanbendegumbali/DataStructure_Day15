d0a34ff36ad1363a3e928990900325beffee3b21
mohan bendegumbali <mohan0311@hotmail.com>
2023-03-16 19:23:37 +0530

    Frequency Of Words In Paragraph

--- HashTables.java
+++ HashTables.java
@@ -3,26 +3,27 @@ package com.hashtables;
 import java.util.Hashtable;
 
 public class HashTables {
-    public static void main(String[] args) {
-        String sentence = "To be or not to be";
-        Hashtable<String, Integer> freq = new Hashtable<>();
+    String sentence = "To be or not to be";
+    Hashtable<String, LinkedList<Integer>> wordFreq = new Hashtable<>();
 
-        // Split the sentence into words
-        String[] words = sentence.split(" ");
+    String[] words = sentence.toLowerCase().split("\\W+");
 
-        // Count the frequency of each word
         for (String word : words) {
-            if (freq.containsKey(word)) {
-                freq.put(word, freq.get(word) + 1);
-            } else {
-                freq.put(word, 1);
-            }
+        LinkedList<Integer> freqList = wordFreq.get(word);
+        if (freqList == null) {
+            freqList = new LinkedList<>();
+            freqList.add(1);
+            wordFreq.put(word, freqList);
+        } else {
+            freqList.add(freqList.getLast() + 1);
         }
-
-        // Print the frequency of each word
-        for (String word : freq.keySet()) {
-            System.out.println(word + ": " + freq.get(word));
-        }
-
     }
+        
+        for (String word : wordFreq.keySet()) {
+        LinkedList<Integer> freqList = wordFreq.get(word);
+        int frequency = freqList.getLast();
+        System.out.println(word + ": " + frequency);
+    }
+
+}
 }
