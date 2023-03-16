3975f1223db9bb3a134ecc8bb641d166d4f3bd04
mohan bendegumbali <mohan0311@hotmail.com>
2023-03-16 19:31:00 +0530

    Remove Word Avoidable

--- HashTables.java
+++ HashTables.java
@@ -1,29 +1,72 @@
 package com.hashtables;
 
-import java.util.Hashtable;
+import java.util.LinkedList;
 
-public class HashTables {
-    String sentence = "To be or not to be";
-    Hashtable<String, LinkedList<Integer>> wordFreq = new Hashtable<>();
+public class RemoveAvoidableWord {
 
-    String[] words = sentence.toLowerCase().split("\\W+");
+    public static void main(String[] args) {
+        String phrase = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        String[] words = phrase.split(" ");
 
+        // Create a LinkedList of MyMapNode objects
+        LinkedList<MyMapNode> wordList = new LinkedList<>();
+
+        // Add each word to the wordList with its count as the value
         for (String word : words) {
-        LinkedList<Integer> freqList = wordFreq.get(word);
-        if (freqList == null) {
-            freqList = new LinkedList<>();
-            freqList.add(1);
-            wordFreq.put(word, freqList);
-        } else {
-            freqList.add(freqList.getLast() + 1);
+            MyMapNode node = null;
+            for (MyMapNode n : wordList) {
+                if (n.getKey().equals(word)) {
+                    node = n;
+                    break;
+                }
+            }
+            if (node == null) {
+                node = new MyMapNode(word, 1);
+                wordList.add(node);
+            } else {
+                node.setValue(node.getValue() + 1);
+            }
+        }
+
+        // Remove the word "avoidable" from the wordList
+        for (MyMapNode node : wordList) {
+            if (node.getKey().equals("avoidable")) {
+                wordList.remove(node);
+                break;
+            }
+        }
+
+        // Print the updated phrase
+        StringBuilder result = new StringBuilder();
+        for (MyMapNode node : wordList) {
+            for (int i = 0; i < node.getValue(); i++) {
+                result.append(node.getKey()).append(" ");
+            }
+        }
+        System.out.println(result.toString().trim());
+    }
+
+    // Custom class to represent a key-value pair
+    static class MyMapNode {
+        private String key;
+        private int value;
+
+        public MyMapNode(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
         }
     }
-        
-        for (String word : wordFreq.keySet()) {
-        LinkedList<Integer> freqList = wordFreq.get(word);
-        int frequency = freqList.getLast();
-        System.out.println(word + ": " + frequency);
-    }
+}
 
-}
-}
